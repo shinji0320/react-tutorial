c2cdbc25fb4d4e3e2eb9009f74e6a7484acf2152
shinji0320 <shinji.blue.one@gmail.com>
2021-03-15 15:35:31 +0900

    onClickでtabの切り替え

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import { useState } from 'react';
 import { List } from "./List";
+import { Form } from "./Form";
 
 
 function App() {
   const [description, setDescription] = useState('クリック前の表示');
+  const [tab, setTab] = useState('list');
 
   const changeDescription = () => {
     setDescription('クリック後の表示です');
   }
   return (
     <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab('list')}>リスト</li>
+          <li onClick={() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr/>
       { description }
-      <List title="取り扱い言語一覧"/>
+      {
+        tab === 'list' ? <List title="取り扱い言語一覧"/> : <Form/>
+      }
       <button onClick={changeDescription}>ボタン</button>
     </div>
   );
