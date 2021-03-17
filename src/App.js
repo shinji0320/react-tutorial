f53d88781a1238b6d0aab03892cb321ede1af993
shinji0320 <shinji.blue.one@gmail.com>
2021-03-17 09:34:30 +0900

    テーマ変更ボタン作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from "./const/languages";
 import { withLoading } from "./hoc/withLoading";
 // import { Modal } from "./components//modal";
+import styled from 'styled-components';
+import { ThemeContext } from './contexts/ThemeContext';
 import { Header } from "./Header";
 
+const Container = styled.div`
+  height: 100%;
+  color: ${({ theme }) => theme.color };
+  background-color: ${({ theme }) => theme.backgroundColor};
+`
 
 function App({ data }) {
   const [tab, setTab] = useState('list');
   const [langs, setLangs] = useState(data);
+
+  const [theme] = useContext(ThemeContext);
 
   const addLang = (lang) => {
     setLangs([...langs, lang]);
@@ -17,13 +26,13 @@ function App({ data }) {
   };
 
   return (
-    <div>
+    <Container theme={theme}>
       <Header tab={tab} setTab={setTab} />
         {
           tab === 'list' ? <List langs={langs}/> : <Form onAddLang={addLang}/>
         }
-    </div>
+    </Container>
   );
 }
 
-export default withLoading(App, getLanguages);
+export default App;
